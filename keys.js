86c91aaedbcdebde0b71c1bf2d52384cf473a058
pdwilliamson5 <30446985+pdwilliamson5@users.noreply.github.com>
2018-02-05 00:38:00 -0600

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,15 @@
+
+console.log("this is loaded");
+
+exports.twitterKeys = {
+  consumer_key: 'sm8icSj7tGZycneMGngIgN69Y',
+  consumer_secret:'xutv94E08dgcuihIltVfzSQw4GcgwIj8w5HnSYbKJq1y8EayvK',
+
+  access_token_key:'119201810-miLdhtdqYfyVH3alBffFPDF88BpQh9ckdrR1rpuT',
+  access_token_secret:'UuLdUbfDWd6MBQLPf0a8SbjMKHcs38EAAQHPyvVXKWtmS',
+};
+
+exports.spotifyKeys = {
+    id: 'e739697c536b498b976c446eabafc6c5',
+    secret: '0f20fa4b2e474e40a6b1c9e14dfd6c19'
+};
